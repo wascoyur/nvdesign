7c2a72095dccbfe2b14f22d6d81d39de1d20bf31
wascoyur <mytem20007@gmail.com>
2021-11-21 16:22:46 +0500

    add global header + bootrstrap

--- components/header/Layout.jsx
+++ components/header/Layout.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
+import Header from "./Header";
 
-const Layout = ({children}) => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+const Layout = ({ children }) => {
+  return (
+    <div>
+      <Header></Header>
+      {children}
+    </div>
+  );
+};
 
-export default Layout
+export default Layout;
