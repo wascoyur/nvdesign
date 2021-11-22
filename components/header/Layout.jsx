3fc4f99852aa077f3f39706548147fb33e244ba1
wascoyur <mytem20007@gmail.com>
2021-11-22 20:02:54 +0500

    добавлен хедер и навбар глобальный

--- components/header/Layout.jsx
+++ components/header/Layout.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import NavbarM from "../navigation/Navbar";
 import Header from "./Header";
 
 const Layout = ({ children }) => {
   return (
-    <div>
-      <Header></Header>
+    <div className="container-fluid px-0">
+      <Header />
+      <NavbarM />
       {children}
     </div>
   );
