2e20dbaa4d69bedb80134a59173e5e85f27f53e3
wascoyur <mytem20007@gmail.com>
2021-12-04 02:08:52 +0500

    registration complete

--- pages/auth/complete.jsx
+++ pages/auth/complete.jsx
@@ -1,16 +1,21 @@
 import React, { Fragment, useEffect, useState } from "react";
 import Header from "next/head";
-import { confirm } from "functions/firebase/confirmEmailRegistration";
+import { signInWithEmailLink, updatePassword } from "@firebase/auth";
+import { auth } from "config/firebase";
+import { toast } from "react-toastify";
+import { useRouter } from "next/dist/client/router";
 
 const Completed = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
   const btnClassName =
-    email && password && password.length > 6
+    email && password && password.length > 6 && !loading
       ? "btn btn-outline-primary"
       : "btn btn-outline-primary disabled";
   const handleInput = (e) => {
-    console.log({ e });
+    // console.log({ e });
     switch (e.target.name) {
       case "login":
         setEmail(e.target.value);
@@ -23,8 +28,33 @@ const Completed = () => {
         break;
     }
   };
-  const handleRegister = (e) => {
-      confirm()
+  const handleRegister = async (e) => {
+    setLoading(true);
+    await signInWithEmailLink(auth, email)
+      .then((res) => {
+        if (!res.user.emailVerified) throw new Error();
+        // console.log({ res });
+        window.localStorage.removeItem("emailForRegister");
+        return auth.currentUser;
+      })
+      .then((currentUser) => {
+        updatePassword(currentUser, password);
+        // console.log({ updatedUser });
+        return currentUser;
+      })
+      .then((updatedUser) => {
+        const idTokenResult = updatedUser.getIdTokenResult();
+        return idTokenResult;
+      })
+      .then((token) => {
+        console.log({ token });
+        // router.push("/");
+      })
+      .catch((err) => {
+        toast.error(`Завершение регистрации завершилось неудачно.
+      Попробуйте еще раз`);
+      });
+    setLoading(false);
   };
   useEffect(() => {
     setEmail(window.localStorage.getItem("emailForRegister"));
